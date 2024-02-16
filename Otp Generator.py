b3280c20e6e00cb4b2f8b7fc63716a52d3841cc9
Prithviraj Verma <158475286+PR-ODINSON@users.noreply.github.com>
2024-02-16 15:21:28 +0530

    Update Otp Generator.py

--- Otp Generator.py
+++ Otp Generator.py
@@ -1,9 +1,20 @@
+# Import necessary libraries
 import random
 import string
-lenght=int(input("Enter the lenght of the password: "))
-def generate_password(length):
-  special_characters="!@#$%&"
-  password=''.join(random.choices(string.ascii_lowercase+string.digits+special_characters,k=lenght))
-  print(password)
 
-generate_password(lenght)
+# Get user input for the desired length of the password
+length = int(input("Enter the length of the password: "))
+
+# Define a function to generate a random password
+def generate_password(length):
+    # Define special characters that can be included in the password
+    special_characters = "!@#$%&"
+    
+    # Generate a random password using lowercase letters, digits, and special characters
+    password = ''.join(random.choices(string.ascii_lowercase + string.digits + special_characters, k=length))
+    
+    # Print the generated password
+    print(password)
+
+# Call the generate_password function with the specified length
+generate_password(length)
